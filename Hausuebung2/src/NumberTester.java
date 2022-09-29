fff612d2fa13b4cb004719ba5677d19b35d50907
PhilippOberbauer <OberbauerP190207@sus.htl-grieskirchen.at>
2022-09-29 10:08:19 +0200

    created interface NumberTest

--- Hausuebung2/src/NumberTester.java
+++ Hausuebung2/src/NumberTester.java
@@ -1,2 +1,26 @@
 public class NumberTester {
+    private String fileName;
+    private NumberTest oddTester;
+    private NumberTest primeTester;
+    private NumberTest palindromeTester;
+
+    public NumberTester(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public void setOddEvenTester(NumberTest oddTester) {
+        this.oddTester = oddTester;
+    }
+
+    public void setPrimeTester(NumberTest primeTester) {
+        this.primeTester = primeTester;
+    }
+
+    public void setPalindromeTester(NumberTest palindromeTester) {
+        this.palindromeTester = palindromeTester;
+    }
+
+    public void testFile() {
+
+    }
 }
